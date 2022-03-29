05c3e6d5ab87ffb95c091e56cde4295c9264710e
Fonya_Brandone <aminbrandone10@gmail.com>
2022-03-29 12:04:10 +0100

    Saved modified changes!

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-var input = document.getElementById('input').value;
+var input = document.getElementById('input');
 var btn = document.getElementById('submit');
 
 btn.addEventListener("click", function() {
-    alert(input);
+    alert(input.value);
 });
